123c44b5b6249944e5831f288c1c3ec96e591b82
Mark <dtphase@gmail.com>
2019-10-16 13:46:16 +1300

    Added react code to display different coloured characters as they come out of the autotyper

--- public_html/scripts/typer.js
+++ public_html/scripts/typer.js
@@ -10,49 +10,151 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 var e = React.createElement;
 
-function letters(props) {
-  return;
-}
+//Props: colour
 
-var Typer = function (_React$Component) {
-  _inherits(Typer, _React$Component);
+var Character = function (_React$Component) {
+  _inherits(Character, _React$Component);
+
+  function Character() {
+    _classCallCheck(this, Character);
+
+    return _possibleConstructorReturn(this, (Character.__proto__ || Object.getPrototypeOf(Character)).apply(this, arguments));
+  }
+
+  _createClass(Character, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'span',
+        { style: { color: this.props.colour } },
+        this.props.character
+      );
+    }
+  }]);
+
+  return Character;
+}(React.Component);
+
+var Typer = function (_React$Component2) {
+  _inherits(Typer, _React$Component2);
 
   function Typer(props) {
     _classCallCheck(this, Typer);
 
-    var _this = _possibleConstructorReturn(this, (Typer.__proto__ || Object.getPrototypeOf(Typer)).call(this, props));
+    var _this2 = _possibleConstructorReturn(this, (Typer.__proto__ || Object.getPrototypeOf(Typer)).call(this, props));
 
-    _this.state = {
-      typed: '',
-      toType: 'false'
+    var code = '<body class="h-100">\n    <div class="container-fluid h-100">\n        <div class="row h-100">\n                <div class="col-sm h-50" id="site">col-sm</div>\n                <div class="col-sm h-50" id="code">\n    ';
+    var characters = [];
+    var colour = 'red';
+    var blocks = code.split('');
+    console.log(blocks);
+    for (var i = 0; i < blocks.length; i++) {
+      if (blocks[i] === "<") {
+        colour = "gray";
+        characters.push([blocks[i], colour]);
+        colour = "red";
+        continue;
+      }
+      if (blocks[i] === "=" || blocks[i] === "/") {
+        var prevColour = colour;
+        colour = "gray";
+        characters.push([blocks[i], colour]);
+        colour = prevColour;
+        continue;
+      }
+      if (blocks[i] == " " && colour == "red") {
+        colour = "orange";
+      }
+      if (blocks[i] == '"' && colour != "green") {
+        colour = "green";
+        characters.push([blocks[i], colour]);
+        continue;
+      }
+      if (blocks[i] == "\"" && colour == "green") {
+        characters.push([blocks[i], colour]);
+        colour = "orange";
+        continue;
+      }
+      if (blocks[i] == ">") {
+        colour = "gray";
+        characters.push([blocks[i], colour]);
+        colour = "white";
+        continue;
+      }
+      console.log(blocks[i]);
+      characters.push([blocks[i], colour]);
+    }
+    console.log(characters);
+    _this2.state = {
+      typed: [],
+      toType: characters,
+      value: ''
     };
-    return _this;
+    return _this2;
   }
 
   _createClass(Typer, [{
-    key: 'renderText',
-    value: function renderText() {
-      var _this2 = this;
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      $(document).keypress(function (e) {
+        var _this3 = this;
 
-      return React.createElement('button', {
-        onClick: function onClick() {
-          return _this2.handleClick(i);
-        }
+        (function () {
+          return _this3.handleKeyPress();
+        });
       });
     }
   }, {
     key: 'handleClick',
     value: function handleClick() {
-      this.state.toType.substring(0, 5);
+      document.getElementById("typingArea").focus();
     }
+  }, {
+    key: 'handleKeyPress',
+    value: function handleKeyPress() {
+      var typingSpeed = 5;
+      var nextCharacters = this.state.toType.slice(0, typingSpeed);
+      var decorated = [];
+      for (var i = 0; i < nextCharacters.length; i++) {
+        if (nextCharacters[i][0] == "\n") {
+          decorated.push(React.createElement('br', null));
+        } else if (nextCharacters[i][0] == " ") {
+          decorated.push(String.fromCharCode(160));
+        } else {
+          decorated.push(React.createElement(
+            'span',
+            { style: { color: nextCharacters[i][1] } },
+            nextCharacters[i][0]
+          ));
+        }
+      }
+      console.log(nextCharacters);
+      this.setState({
+        typed: this.state.typed.concat(decorated),
+        toType: this.state.toType.slice(typingSpeed)
+      });
+    }
+  }, {
+    key: 'handleChange',
+    value: function handleChange() {}
   }, {
     key: 'render',
     value: function render() {
+      var _this4 = this;
+
       return React.createElement(
-        'p',
+        'div',
         null,
-        this.state.toType,
-        this.renderText()
+        this.state.typed,
+        React.createElement('input', { type: 'text', id: 'typingArea',
+          onKeyPress: function onKeyPress() {
+            return _this4.handleKeyPress();
+          },
+          onChange: function onChange() {
+            return _this4.handleChange();
+          },
+          value: this.state.value
+        })
       );
     }
   }]);
--- src/typer.js
+++ src/typer.js
@@ -1,41 +1,124 @@
 'use strict';
 
+
+
 const e = React.createElement;
 
-function letters(props) {
-    return ;
+
+//Props: colour
+class Character extends React.Component {
+  render() {
+    return <span style={{color:this.props.colour}}>{this.props.character}</span>;
+  }
 }
+
 
 class Typer extends React.Component {
   constructor(props) {
     super(props);
+    const code = `<body class="h-100">
+    <div class="container-fluid h-100">
+        <div class="row h-100">
+                <div class="col-sm h-50" id="site">col-sm</div>
+                <div class="col-sm h-50" id="code">
+    `;
+    var characters = [];
+    var colour = 'red';
+    var blocks = code.split('');
+    console.log(blocks);
+    for(var i=0;i<blocks.length;i++) {
+      if(blocks[i] === "<") {
+        colour = "gray";
+        characters.push([blocks[i], colour]);
+        colour = "red";
+        continue;
+      }
+      if(blocks[i] === "=" || blocks[i] === "/") {
+        let prevColour = colour;
+        colour = "gray";
+        characters.push([blocks[i], colour]);
+        colour = prevColour;
+        continue;
+      }
+      if(blocks[i] == " " && colour == "red") {
+        colour = "orange";
+      }
+      if(blocks[i] == '"' && colour != "green") {
+        colour = "green";
+        characters.push([blocks[i], colour]);
+        continue;
+      }
+      if(blocks[i] == "\"" && colour == "green") {
+        characters.push([blocks[i], colour]);
+        colour = "orange";
+        continue;
+      }
+      if(blocks[i] == ">") {
+        colour = "gray";
+        characters.push([blocks[i], colour]);
+        colour = "white";
+        continue;
+      }
+      console.log(blocks[i]);
+      characters.push([blocks[i], colour]);
+      
+
+    }
+    console.log(characters);
     this.state = { 
-        typed: '',
-        toType: 'false',
+        typed: [],
+        toType: characters,
+        value: '',
     };
   }
 
-  
-
-  renderText() {
-    return (
-      <button
-        onClick={() => this.handleClick(i)}
-      />
+  componentDidMount() {
+    $(document).keypress(
+      function (e) {
+        () => this.handleKeyPress();
+      }
     );
   }
 
   handleClick() {
-    this.state.toType.substring(0,5);
+    document.getElementById("typingArea").focus(); 
+  }
+
+  handleKeyPress() {
+    const typingSpeed = 5;
+    let nextCharacters = this.state.toType.slice(0,typingSpeed);
+    let decorated = [];
+    for(var i=0;i<nextCharacters.length;i++) {
+      if(nextCharacters[i][0] == "\n") {
+        decorated.push(<br />);
+      } else if(nextCharacters[i][0] == " ") {
+        decorated.push(String.fromCharCode(160));
+      } else {
+        decorated.push(<span style={{color:nextCharacters[i][1]}}>{nextCharacters[i][0]}</span>);
+      }
+      
+    }
+    console.log(nextCharacters);
+    this.setState({
+      typed: this.state.typed.concat(decorated),
+      toType: this.state.toType.slice(typingSpeed),
+    });
+  }
+
+  handleChange() {
 
   }
 
   render() {
     return (
-      <p>
-        {this.state.toType}
-        {this.renderText()}
-      </p>
+      <div>
+        {this.state.typed}
+        <input type="text" id="typingArea" 
+        onKeyPress={() => this.handleKeyPress()}
+        onChange={() => this.handleChange()}
+        value={this.state.value}
+        />
+      </div>
     );
   }
 }
